18d41175d4961acc86a79ef4f2d89b49cf5b503a
EzekR <renatus1812@163.com>
2019-09-26 13:51:26 +0800

    sd

--- lib/models/search_model.dart
+++ lib/models/search_model.dart
@@ -12,10 +12,25 @@ class SearchModel extends Model{
     'guarantee': ''
   };
 
+  List<Map> _selected = [];
+
   get result => _result;
+  get selected => _selected;
 
   void setResult (Map result){
       _result = result;
       notifyListeners();
+  }
+
+  void addToSelected(Map item) {
+    _selected.add(item);
+    print(_selected);
+    notifyListeners();
+  }
+
+  void removeSelected(Map item) {
+    print(item);
+    _selected.remove(item);
+    notifyListeners();
   }
 }
